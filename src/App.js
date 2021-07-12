7bddbd4abb23c94e7692706d9c0c5b489025ac4d
Aditya Pundir <adityapundir2k@gmail.com>
2021-07-12 11:59:39 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 
-function App() {
-  const [order, setOrder] = useState(false);
-
+export default function App() {
   const createOrder = (data, actions) => {
     return actions.order.create({
       purchase_units: [
         {
           amount: {
-            value: "14",
+            value: "10",
           },
         },
       ],
@@ -21,27 +19,11 @@ function App() {
   const onApprove = (data, actions) => {
     return actions.order.capture();
   };
+
   return (
-    <>
-      {order ? (
-        <PayPalButton
-          createOrder={(data, actions) => createOrder(data, actions)}
-          onApprove={(data, actions) => {
-            onApprove(data, actions);
-            setOrder(false); // For paymentBtnCheckout, it will setOrder to false only when the payment is completed.
-          }}
-        />
-      ) : (
-        <button // PaymentBtnCheckout
-          onClick={() => {
-            setOrder(true);
-          }}
-        >
-          Pay Now
-        </button>
-      )}
-    </>
+    <PayPalButton
+      createOrder={(data, actions) => createOrder(data, actions)}
+      onApprove={(data, actions) => onApprove(data, actions)}
+    />
   );
 }
-
-export default App;
